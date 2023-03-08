28dfffbdafa0a6de06a0afe86326186b0c67e6ca
singhabhi3290 <118481506+singhabhi3290@users.noreply.github.com>
2023-03-08 16:56:12 +0530

    Update 1. check which number is greater between 2 numbers.cpp

--- 2nd sheet/1. check which number is greater between 2 numbers.cpp
+++ 2nd sheet/1. check which number is greater between 2 numbers.cpp
@@ -3,10 +3,10 @@ int main() {
 	int a,b;
 	scanf("%d%d",&a,&b);
 	if(a>b){
-		printf("a is greater",&a);
+		printf("a is greater");
 	}
 	if(a<b){
-		printf("b is greater",&b);
+		printf("b is greater");
 	}
 	if(a==b){
 		printf("Both are equal");
